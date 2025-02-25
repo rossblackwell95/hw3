73762aa47591be08639e74af581b692f3d37dd3a
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-02-25 17:33:39 +0000

    Deleted database with test data

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,5 +5,12 @@ class EntriesController < ApplicationController
   end
 
   def create
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    @entry.save
+    redirect_to "/places/#{params["place_id"]}"
   end
 end
