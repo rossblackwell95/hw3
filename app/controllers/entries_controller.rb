9f9b376e4761e773d90c9484145add9e367b0e34
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-02-25 17:03:54 +0000

    Set up controllers and first draft HTML

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,7 @@
 class EntriesController < ApplicationController
   def new
+    @entry = Entry.new
+    @place = Place.find_by({ "id" => params["place_id"] })
   end
 
   def create
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
   def index
+    @places = Place.all
   end
 
   def show
@@ -9,5 +10,9 @@ class PlacesController < ApplicationController
   end
 
   def create
+    @place = Place.new
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
   end
 end
